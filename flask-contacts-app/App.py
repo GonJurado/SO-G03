dc10b486429b21d235ff1337392234d9fcd0a495
QuispeAyalaDiego <u202020065@upc.edu.pe>
2023-04-30 00:04:58 -0500

    All last changes
    Trabajo parcial fin

--- flask-contacts-app/App.py
+++ flask-contacts-app/App.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 
 # MySQL connection
-app.config['MYSQL_HOST'] = '44.211.143.40'
+app.config['MYSQL_HOST'] = '54.208.66.149'
 app.config['MYSQL_USER'] = 'support'
 app.config['MYSQL_PASSWORD'] = 'cub'
 app.config['MYSQL_DB'] = 'socrud'
